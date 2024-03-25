fa6c18045daf473cc233105d4562d94d82bc3b62
German Ivanov <germanivanov0719@gmail.com>
2024-03-25 21:18:02 +0300

    Change style and type of buttons
    I hope it helps pass the tests…

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+//    @IBAction func buttonPressed(_ sender: UIButton) {
+//        guard let buttonText = sender.currentTitle else {return}
+//        print(buttonText)
+//    }
+    
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
-        guard let buttonText = sender.currentTitle else {return}
-        print(buttonText)
+        guard let buttonText = sender.titleLabel else {
+            print("nil")
+            return
+        }
+        print(buttonText.text!)
+        
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
