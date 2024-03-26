a5d35cdbafa833a90101c39a22461a6ce79e8e33
German Ivanov <germanivanov0719@gmail.com>
2024-03-26 03:01:59 +0300

    i have no idea about what's wrong with this project

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -8,26 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-//    @IBAction func buttonPressed(_ sender: UIButton) {
-//        guard let buttonText = sender.currentTitle else {return}
-//        print(buttonText)
-//    }
-    
-    
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        guard let buttonText = sender.titleLabel else {
-            print("nil")
-            return
-        }
-        print(buttonText.text!)
-        
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.titleLabel?.text else { return }
+        print(buttonText)
+    }
+    
+    
+    //    @IBAction func buttonPressed(_ sender: UIButton) {
+    //        guard let buttonText = sender.titleLabel else {
+    //            print("nil")
+    //            return
+    //        }
+    //        print(buttonText.text!)
+    //
+    //    }
     
 }
 
